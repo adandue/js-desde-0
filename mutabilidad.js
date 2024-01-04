29026979661fd67e917305a0d11c1d004b86f617
adan_due <ing.adanduenas@gmail.com>
2024-01-04 16:29:22 -0600

    Tipos de datos: mutabilidad e inmutabilidad

---
+++ mutabilidad.js
@@ -0,0 +1,30 @@
+// tipo de dato primitivo - Inmutable
+let numero = 23
+numero = numero + 10
+console.log(numero)
+
+let esVerdadero = true
+esVerdadero = false
+console.log(esVerdadero)
+
+// tipo de dato complejo - mutable
+let usuario = {
+    nombre: 'Pepito',
+    edad: 15
+}
+usuario.edad = 20
+console.log(usuario)
+
+let frutas = ['manzana', 'pera']
+frutas[0] = 'sandia'
+console.log(frutas)
+
+const cambiarNombre = (objeto) => objeto.nombre = 'Nuevo nombre'
+
+let persona = {
+    nombre: 'Antonio'
+}
+
+cambiarNombre(persona)
+
+console.log(persona)

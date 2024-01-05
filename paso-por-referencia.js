6dce36e15431d15e46c3b0d578ba7df48ea27478
adan_due <ing.adanduenas@gmail.com>
2024-01-05 12:07:32 -0600

    paso por referencia

---
+++ paso-por-referencia.js
@@ -0,0 +1,17 @@
+//Tipo de dato complejo Paso por referencia
+let frutas = {
+    naranja: '🍊'
+}
+let vegetales = frutas
+
+console.log(vegetales)
+
+vegetales.naranja = '🥦'
+console.log(frutas)
+
+let ropa = {
+    blusa: '👚'
+}
+
+ropa.pantalon = '👖'
+console.log(ropa)

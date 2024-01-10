34aaf69bfba35658824dfe3cf1b9c8c5d6bd48fa
adan_due <ing.adanduenas@gmail.com>
2024-01-10 11:45:27 -0600

    anatomía de una función

---
+++ anatomia-funcion.js
@@ -0,0 +1,13 @@
+const calculateDiscountedPrice = (price, discountPercentage) => {
+    const discount = (price * discountPercentage)/100
+    const priceWithDiscount = price - discount
+    return priceWithDiscount
+}
+
+const originalPrice = 1500
+const discountPercentage = 15
+const finalPrice = calculateDiscountedPrice(originalPrice, discountPercentage)
+
+console.log('Original Price: $' + originalPrice)
+console.log('Discount: ' + discountPercentage + '%')
+console.log('Price with discount: $' + finalPrice)

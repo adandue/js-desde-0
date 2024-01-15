77c1981468fd610bf5fea87f276b53fadacf4390
adan_due <ing.adanduenas@gmail.com>
2024-01-15 17:46:26 -0600

    Crea biografias de personajes con JavaScript

---
+++ ejercicio-practico-funciones.js
@@ -0,0 +1,37 @@
+// Create PowerpuffGirl objects
+function PowerpuffGirl (name, color, superpower) {
+    this.name = name
+    this.color = color
+    this.superpower = superpower
+    this.isLeader = false
+
+    this.displayInfo = function () {
+        console.log(`
+            Powerpuff Girl Information:
+            Name: ${this.name}
+            Color: ${this.color}
+            Superpowe: ${this.superpower}
+            ${this.isLeader ? 'Leader: Yes' : 'Leader: No'}
+        `)
+    }
+    this.becomeLeader = function () {
+        this.isLeader = true
+        console.log(`
+            ${this.name} has become the leader od the Powerpuff Girls
+        `)
+    }
+}
+
+const blossom = new PowerpuffGirl('Blossom', 'Pink', 'Ice Breath')
+const buttercup = new PowerpuffGirl('Buttercup', 'Green', 'Super Strength')
+const bubbles = new PowerpuffGirl('Bubbles', 'Blue', 'Flight')
+
+blossom.displayInfo()
+buttercup.displayInfo()
+bubbles.displayInfo()
+
+bubbles.becomeLeader()
+
+blossom.displayInfo()
+buttercup.displayInfo()
+bubbles.displayInfo()

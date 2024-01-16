ce7abf287fded5ab7795f1fe38010058b83d1564
adan_due <ing.adanduenas@gmail.com>
2024-01-16 12:17:56 -0600

    Operadores lógicos

---
+++ ciclos/operadores/logica.js
@@ -0,0 +1,17 @@
+/*
+
+Operadores Lógicos
+
+&& AND
+|| OR
+! NOT
+
+*/
+
+const a = 10
+const b = 20
+const c = '10'
+
+console.log(a == b && a === c)
+console.log(a != b || a === c)
+console.log(!(a === c ))

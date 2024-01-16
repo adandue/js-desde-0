ac5d3ae829742dcb021968469f5720d86181e172
adan_due <ing.adanduenas@gmail.com>
2024-01-16 12:11:43 -0600

    Operadares de comparación

---
+++ ciclos/operadores/index.js
@@ -0,0 +1,28 @@
+/*
+
+Operadores de comparación
+
+== nos ayudan a comparar si algo es igual a otra cosa
+=== nos ayuda a comparar algo en dos temas tanto en valor como en tipo de dato
+!= Si es diferente se genera true
+!== Es diferente en el valor y en el tipo de dato
+> Si un valor es mayor que otro valor
+< Si un valor es menor que otro valor
+>= Si es mayor o igual
+<= Si es menor o igual
+
+*/
+
+const a = 10
+const b = 20
+const c = '10'
+
+console.log(a == b)
+console.log(a == c)
+console.log(a === c)
+console.log(a != c)
+console.log(a !== c)
+console.log(a > b)
+console.log(a > c)
+console.log(a <= b)
+console.log(a <= c)

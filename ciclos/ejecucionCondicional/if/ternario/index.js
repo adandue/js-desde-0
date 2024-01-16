ab2a09b256fe84191f225c71d38ffa9d9c71fa83
adan_due <ing.adanduenas@gmail.com>
2024-01-16 13:55:38 -0600

    Ejecunción condicional: Ternario

---
+++ ciclos/ejecucionCondicional/if/ternario/index.js
@@ -0,0 +1,23 @@
+// let mensaje
+
+// const edad = 17
+
+// if (edad >= 18) {
+//     mensaje = 'Es mayor de edad. Puede pasar'
+// } else {
+//     mensaje = 'No es mayor de edad. No puede pasar'
+// }
+
+// console.log(mensaje)
+
+/*
+Estructura del ternario
+
+    condición ? true : false
+*/
+
+const age = 17
+
+const message = age >= 18 ? 'Es mayor de edad. puede pasar' : 'Es menor de edad. No puede pasar'
+
+console.log(message)

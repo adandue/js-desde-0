4c13c7578a1149567a7adfa2056597c973f953b6
adan_due <ing.adanduenas@gmail.com>
2024-01-16 12:45:03 -0600

    Ejecición condicional: if

---
+++ ciclos/ejecucionCondicional/if/index.js
@@ -0,0 +1,26 @@
+/*
+if (algo === algo) {
+    código
+} else {
+    otro código
+}
+
+if (algo === algo) {
+    código
+} else if (algo != algo) {
+    otro código
+} else {
+
+}
+
+*/
+
+let nombre = 'Adan'
+
+if (nombre === 'Nico') {
+    console.log('Bienvenido Nico')
+} else if (nombre === 'Adan') {
+    console.log('Bienvenido Adan')
+} else {
+    console.log('El nombre no fue encontrado')
+}

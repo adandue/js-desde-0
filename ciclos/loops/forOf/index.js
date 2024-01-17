624be0b534417baa673b1944f797285749410cf9
adan_due <ing.adanduenas@gmail.com>
2024-01-17 15:24:00 -0600

    loop for of

---
+++ ciclos/loops/forOf/index.js
@@ -0,0 +1,17 @@
+/*
+
+Se utiliza en objetos iterables
+
+for of  arrays, strings [algo]
+
+for(variable of objeto) {
+    código a ejecutar
+}
+
+*/
+
+let canasta = ['manzana', 'pera', 'naranja', 'uva']
+
+for (fruta of canasta) {
+    console.log(fruta)
+}

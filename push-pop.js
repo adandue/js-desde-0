00f61a75df10b616c28b10dbc80c6e9fc7c1328f
adan_due <ing.adanduenas@gmail.com>
2024-01-19 13:09:16 -0600

    push pop

---
+++ push-pop.js
@@ -0,0 +1,13 @@
+// Methods that modify the original array (Mutability)
+
+//push()
+const countries = ['USA', 'Canada', 'UK']
+const newCountries = countries.push('Gernamany', 'Australia')
+
+console.log(countries)
+console.log(newCountries)
+
+//pop()
+const removeCountry = countries.pop()
+console.log(countries)
+console.log(newCountries)

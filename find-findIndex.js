91e095f586e17e2addfad3dc0466f1d6f73b7712
adan_due <ing.adanduenas@gmail.com>
2024-01-23 14:57:44 -0600

    Ganador de una rifa

--- find-findIndex.js
+++ find-findIndex.js
@@ -14,3 +14,44 @@ const indexNumber = randomNumbers.findIndex(number => number > 50)
 
 console.log(randomNumbers)
 console.log(indexNumber)
+
+/*
+Exercise: Raffle Winner Verification Program
+
+In this program, you can verify if a person is among the list of winners in a raffle. Simply input the name or ticket number, and the program will check and display the winner's information
+*/
+
+const winningParticipants = [
+    { id: 1, name: 'Adan', ticketNumber: 857 },
+    { id: 65, name: 'Vero', ticketNumber: 325 },
+    { id: 72, name: 'Aurora', ticketNumber: 412 },
+    { id: 98, name: 'Luis', ticketNumber: 623 },
+    { id: 123, name: 'Pipis', ticketNumber: 789 },
+    { id: 145, name: 'Sofi', ticketNumber: 32 },
+    { id: 278, name: 'Luis Jr', ticketNumber: 2 },
+    { id: 346, name: 'Alo', ticketNumber: 25 }
+]
+
+const findWinnerByName = (name) => {
+    const winner = winningParticipants.find(participants => participants.name === name)
+    return winner || 'No winner found with that name'
+}
+
+const findIndexWinnerByTicket = (ticketNumber) => {
+    const index = winningParticipants.findIndex(participants => participants.ticketNumber === ticketNumber)
+    return index !== -1 ? index : 'No winner found with that ticket number'
+}
+
+const displayWinnerInformation = (winner) => {
+    if (winner != undefined && winner != null && winner != 'No winner found with that name') {
+        console.log('Winner information: ', winner)
+    } else {
+        console.log('No winner found')
+    }
+}
+
+const winnerByName = findWinnerByName('Sofi')
+const indexWinnerByTicket = findIndexWinnerByTicket(623)
+
+displayWinnerInformation(winnerByName)
+console.log('Index of Winner by Ticket Number: ', indexWinnerByTicket)

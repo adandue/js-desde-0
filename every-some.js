8484ce06960b0cb6e63a85667c06b958a3d82e93
adan_due <ing.adanduenas@gmail.com>
2024-01-26 13:40:18 -0600

    every some

---
+++ every-some.js
@@ -0,0 +1,13 @@
+//Methods that DO NOT modify the original array (Immutability)
+
+const ages = [21, 25, 30, 19, 22]
+
+//every()
+const allAreAdults = ages.every(age => age > 20)
+
+console.log(ages)
+console.log(allAreAdults)
+
+//some()
+const atLeastOneISOver30 = ages.some(age => age >= 30)
+console.log(atLeastOneISOver30)
